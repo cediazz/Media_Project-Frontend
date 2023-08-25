40f88eca352bd49e2b5e41c1d1fb8150ba80358a
cediazz <dce92179@gmail.com>
2023-08-25 16:32:19 -0400

    FieldsManagementComponentv1ANDFieldsComponentv1

--- src/Componentes/Fields/Fields.jsx
+++ src/Componentes/Fields/Fields.jsx
@@ -3,17 +3,13 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import {BsFillTrashFill} from 'react-icons/bs';
 
-export default function Field () {
+export default function Field (props) {
   return (
-    <>
+    
        <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-        />
-        <Button variant="outline-danger"><BsFillTrashFill /> Eliminar</Button>
+        <Form.Control  required type="text"/>
+        <Button variant="outline-danger" onClick={() => props.deleteField(props.index)}><BsFillTrashFill /> Eliminar</Button>
       </InputGroup>
-    </>
+    
   );
 }
--- src/Componentes/FieldsManagement/FieldsManagement.jsx
+++ src/Componentes/FieldsManagement/FieldsManagement.jsx
@@ -5,13 +5,15 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import {BsFillSignIntersectionFill} from 'react-icons/bs';
-
+import { BsFillSignIntersectionFill } from 'react-icons/bs';
+import Field from "../Fields/Fields";
 
 
 export default function FieldsManagement() {
-  const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState(false)
   const [categorySelected, setCategorySelected] = useState("Seleccionar la Categoría")
+  const [fields, setFields] = useState([])
+
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -23,13 +25,38 @@ export default function FieldsManagement() {
     setValidated(true);
   };
 
+  const deleteField = (index) => {
+    //console.log(fields.length)
+    //console.log(index)
+
+    const newFields = [...fields]
+    newFields.splice(index, 1)
+    setFields(newFields)
+    console.log(newFields.length)
+    console.log(newFields)
+
+
+  };
+
+  const addFields = () => {
+    //setFields([fields, <Field deleteField={deleteField} index={fields.length}/>]);
+    const fieldss = [...fields]
+    fieldss.push(<Field deleteField={deleteField} index={fields.length} />)
+    setFields(fieldss)
+    //console.log(fields)
+
+  }
+
+
+
   return (
+
     <Container className="border mt-5">
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Row className="mb-3 mt-3">
-        <Form.Label>Seleccione la Categoría</Form.Label>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-            
+          <Form.Label>Seleccione la Categoría</Form.Label>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+
             <Form.Select aria-label="Default select example" required onChange={e => setCategorySelected(e.target.value)}>
               <option selected disabled>Seleccionar la Categoría</option>
               <option >Categorias</option>
@@ -39,15 +66,25 @@ export default function FieldsManagement() {
             <Form.Control.Feedback type="invalid">Por favor seleccione la Categoría</Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="4" controlId="validationCustom01">
-           {categorySelected != "Seleccionar la Categoría" && <Button className="mb-3" type="submit" variant="outline-primary"><BsFillSignIntersectionFill /> Adicionar Campos</Button>}
+            {categorySelected != "Seleccionar la Categoría" && <Button className="mb-3" onClick={addFields} variant="outline-primary"><BsFillSignIntersectionFill /> Adicionar Campos</Button>}
           </Form.Group>
-       </Row>
-        <Row>
-          
         </Row>
-        
+        <Row>
+          {fields && fields.map((field, index) => (
+            <Form.Group as={Col} md="4" controlId="validationCustom02">
+              {field}
+            </Form.Group>
+          ))}
+        </Row>
+
       </Form>
+
+      {fields.length}
     </Container>
+
+
+
+
   );
 
 
